09a0a1708af6c7ce71cbabbd59af8a74df817709
Ding <ding.chen@cloud-interactive.com>
2020-09-13 12:03:03 +0800

    Fixed wrong words

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,16 +1,16 @@
-'use strict';
-const path = require('path');
-const assert = require('yeoman-assert');
-const helpers = require('yeoman-test');
+"use strict";
+const path = require("path");
+const assert = require("yeoman-assert");
+const helpers = require("yeoman-test");
 
-describe('generator-next-charkra-typscript:app', () => {
+describe("generator-next-chakra-typescript:app", () => {
   beforeAll(() => {
     return helpers
-      .run(path.join(__dirname, '../generators/app'))
+      .run(path.join(__dirname, "../generators/app"))
       .withPrompts({ someAnswer: true });
   });
 
-  it('creates files', () => {
-    assert.file(['dummyfile.txt']);
+  it("creates files", () => {
+    assert.file(["dummyfile.txt"]);
   });
 });
